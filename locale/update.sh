5466b15c09c4f8fa1fb78786698f42c4baea5b6b
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-08-22 19:15:18 +0800

    Update workflows

--- locale/update.sh
+++ locale/update.sh
@@ -14,8 +14,8 @@ sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
 sphinx-build -T -b gettext ../abqpy/docs/source pot
 sphinx-intl update-txconfig-resources -p pot -d .
-cat .tx/config
-tx push -s --skip
-rm -R -f zh_CN
-tx pull -l zh_CN
-git checkout .tx/config
+# cat .tx/config
+# tx push -s --skip
+# rm -R -f zh_CN
+# tx pull -l zh_CN
+# git checkout .tx/config
